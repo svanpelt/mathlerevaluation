2e9aabc384e58b53999ad6bf949c585db11de97d
Samuel Van Pelt <samuelvanpelt@gmail.com>
2022-02-05 10:55:00 -0800

    resolve counting bug where some equations with 0 in them were not included

--- mathler.js
+++ mathler.js
@@ -1,6 +1,6 @@
 
 const symbols = ['-', '+', '/', '*'];
-const zero_preceder = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 const valid_entries = {
 	0: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'],
 	1: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '+', '/', '*'],
@@ -16,13 +16,31 @@ function create_valid_equations() {
 	let results = [];
 	nest_valid_symbols(0, results, "");
 	let evals = results.map(eq => eval(eq));
-	let counts = {};
-	evals.forEach(value => {
+	let valid = {};
+	results.forEach(eq => {
+		let value = eval(eq);
 		if (value < 100 && value > -1 && Number.isInteger(value)) {
-			counts[value] = counts[value] != null ? counts[value] + 1 : 1;
+			if (valid[value] != null) {
+				let newArray = valid[value];
+				valid[value].push(eq);
+			} else {
+				valid[value] = [eq];
+			}
 		}
 	});
+	let counts = {};
+	for (const [key, value] of Object.entries(valid)) {
+		counts[key] = value.length;
+	}
 	console.log(JSON.stringify(counts));
+	/*for (let i = 20; i< 35; i++) {
+		let key = Math.floor(Math.random() * (Object.keys(valid).length - 1));
+		let list = valid[key];
+		for (let j = 0; j < 10; j ++) {
+			answer = list[Math.floor(Math.random() * list.length - 1)];
+			filter_valid_constraints(list, [list[Math.floor(Math.random() * list.length - 1)], answer], answer);
+		}
+	}*/
 }
 
 function nest_valid_symbols(i, results, start) {
@@ -32,16 +50,161 @@ function nest_valid_symbols(i, results, start) {
 	}
 	let next_valid_symbols = valid_entries[i];
 	for (let j = 0; j< next_valid_symbols.length; j++) {
-		if ((!symbols.includes(next_valid_symbols[j]) || !symbols.includes(start.charAt(start.length - 1))) 
-			&& 
-			(next_valid_symbols[j] != 0 || 
-			 zero_preceder.includes(start.charAt(start.length-1)))) {
-			nest_valid_symbols(i + 1, results, start + next_valid_symbols[j]);
+		let char = next_valid_symbols[j];
+		if (is_valid_char(start, char)) {
+			nest_valid_symbols(i + 1, results, start + char);
 		}
 	}
 
 }
 
-function evaluate(string) {
+function is_valid_char(start, char) {
+  if (symbols.includes(char) && symbols.includes(start.charAt(start.length - 1))) {
+
+  	return false;
+  }
+
+  if (numbers.includes(char)) {
+  	if (number_is_0_prefixed(start + char)) {
+  		return false;
+  	}
+  }
+  return true;
+}
+
+function number_is_0_prefixed(eq) {
+  let number = '';
+  for(let i = 0; i < eq.length; i ++) {
+  	let char = eq.charAt(i);
+  	if (symbols.includes(char)) {
+  		number = '';
+  		continue;
+  	}
+  	number += char;
+  	if (number.length > 1 && number.charAt(0) == '0') {
+  		return true;
+  	}
+  }
+  return false;
+}
+
+
+function filter_valid_constraints(valid, guesses, word, log = true) {
+	if (log === true){
+	  console.log("Checking guess quality for " + word + ", " + valid.length + " options");
+	  // pick_bad_guess(valid, word);
+	}
+	let filtered_valid = valid;
+	let remaining = [];
+	for (guess of guesses) {
+	  filtered_valid = filter_valid_constraints_single(filtered_valid, guess, word);
+	if (log === true){
+		console.log(guess + ": " + filtered_valid.length + " remaining words")
+		//console.log(filtered_valid);
+		// bad_option = pick_bad_guess(filtered_valid, word);
+	}
+	remaining.push(filtered_valid.length);
+	}
+	return remaining
+}
+
+function filter_valid_constraints_single(valid, guess, word) {
+	const constraints = constrain(guess, word)
+	includes = constraints[0];
+	excludes = constraints[1];
+	includes_in_a_different_position = constraints[2];
+
+	let filtered_valid = valid.filter(option => {
+		//
+		for (let i = 0; i < option.length; i++) {
+			if (includes[i] != null && includes[i] !== option[i]) {
+				return false;
+			}
+			if (includes_in_a_different_position[i] != null 
+				&& includes_in_a_different_position[i] === option[i]) {
+				return false;
+			}
+		}
+		//excludes
+		for (char of option) {
+			if (excludes.includes(char)) {
+
+				return false;
+			}
+		}
+		// different position
+		for (required of Object.values(includes_in_a_different_position)) {
+			if (!option.includes(required)) {
+				return false;
+			}
+		}
+
+		return true;
+	});
+	return filtered_valid;
+}
+
+/*
+ * This function creates constraints based upon a guess and the correct word. 
+ * The three constraints are:
+ ** Includes at a specific position (green letters)
+ ** Excludes (black letters)
+ ** Includes at a different position (yellow letters)
+ * These constraints only work on a single word, and past constraints are discarded.
+ * This should work because the valid wordlist should be filtered of all words that 
+ * hit past constraints.
+ */
+function constrain(guess, word) {
+	let filtered_word = word;
+	let filtered_guess = guess;
+	let includes_at_position = {};
+	let excludes = [];
+	let includes_in_a_different_position = {};
+	for (let i = 0; i < guess.length; i++) {
+		if (guess[i] === word[i]) {
+			includes_at_position[i] = guess[i];
+		} else if (!word.includes(guess[i])) {
+			excludes.push(guess[i]);
+		} else if (word.includes(guess[i]) && can_include_another_of_this_letter(guess[i], word, Object.values(includes_at_position), Object.values(includes_in_a_different_position))) {
+			includes_in_a_different_position[i] = guess[i];
+		}
+	}
+	return [includes_at_position, excludes, includes_in_a_different_position];
+}
+
+/* 
+ * This function counts all the letters in the word, as well as the letters in existing
+ * constraints. This allows us to filter doubled letters in guesses that should be yellow.
+ * TODO: there is probably a better way to do this, all of this, what have I written?
+ */
+function can_include_another_of_this_letter(char, word, includes_values, different_values) {
+	let counts_in_word = {};
+	let counts_in_constraints = {};
+	for (letter of word) {
+		if (counts_in_word[letter] != null) {
+			counts_in_word[letter] = counts_in_word[letter] + 1
+		} else {
+			counts_in_word[letter] = 1
+		}
+	}
+	for (letter of includes_values) {
+		if (counts_in_constraints[letter] != null) {
+			counts_in_constraints[letter] = counts_in_constraints[letter] + 1
+		} else {
+			counts_in_constraints[letter] = 1
+		}
+	}
+	for (letter of different_values) {
+		if (counts_in_constraints[letter] != null) {
+			counts_in_constraints[letter] = counts_in_constraints[letter] + 1
+		} else {
+			counts_in_constraints[letter] = 1
+		}
+	}
+	if (counts_in_constraints[char] == null || counts_in_constraints[char] < counts_in_word[char]) {
+		return true;
+	} 
+	return false;
 
 }
+
